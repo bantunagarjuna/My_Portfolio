4a91576a74ae10f56ad1b924af6e105afb8b2028
bantunagarjuna <bantunagarjuna2@gmail.com>
2025-05-14 14:25:16 +0530

    render

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes/index');
+const routes = require('./routes/index'); 
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -20,14 +20,11 @@ app.use('/', routes);
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.status(500).send('Something broke!');  
 });
 
-// Only listen on port if not running in serverless environment
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
-}
+app.listen(PORT, () => { 
+    console.log(`Server is running on http://localhost:${PORT}`); 
+});
 
 module.exports = app;
